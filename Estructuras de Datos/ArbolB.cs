bc2c78b759c2ea948b9c154537cb69df8dd68fdc
Mario991012 <39171404+Mario991012@users.noreply.github.com>
2019-04-30 00:53:08 -0600

    Arbol B terminado, commit correcto

--- Estructuras de Datos/ArbolB.cs
+++ Estructuras de Datos/ArbolB.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -125,6 +125,7 @@ namespace Estructuras_de_Datos
                 }
 
                 Nodo.Padre.Hijos.RemoveAt(indice);
+                Nodo.Padre.Hijos.Sort((x, y) => x.Valores[0].CompareTo(y.Valores[0]));
                 Nodo = null;
             }//Si es la raiz y aun caben valores en el nodo
             else if (Nodo.Padre == null && Nodo.Hijos.Count < 5)
@@ -227,6 +228,10 @@ namespace Estructuras_de_Datos
                 NodoHijo = Nodo.Hijos[PosicionHijo(Nodo, valor)];
                 return Busqueda(valor, NodoHijo);
             }
+            else if(BEncontrado == true)
+            {
+                return val;
+            }
             else if (Nodo.Hijos.Count == 0)
             {
                 throw new NotImplementedException();
@@ -238,45 +243,339 @@ namespace Estructuras_de_Datos
 
         }
 
+        public bool ExisteUnderFlow(NodoB<T> Nodo)
+        {
+            if (Nodo.Valores.Count < Nodo.min)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static int contador = 0;
+        static NodoB<T> NodoAModificar = new NodoB<T>();
+        static NodoB<T> NodoDar = new NodoB<T>();
+
         public void Eliminar(T valor, NodoB<T> Nodo)
         {
-            bool BEncontrado = false;
             int indice = 0;
+            bool NodoInicial = false;
+            bool BEncontrado = false;
             for (int i = 0; i < Nodo.Valores.Count; i++)
             {
-                if(Nodo.Valores[i].CompareTo(valor) == 0)
+                if (Nodo.Valores[i].CompareTo(valor) == 0)
                 {
                     BEncontrado = true;
-                    indice = i;
-                    break;
+                    if (ExisteNodosHijo(Nodo) == true)
+                    {
+                        //if (contador == 0)
+                        //{
+                            indice = i;  //INDICE DE VALOR A SUSTITUIR
+                            NodoAModificar = Nodo; //NODO A SUSTITUIR VALOR DE NODO MAS IZQUIERDO DEL HIJO DERECHO
+                            contador++; //revisar para que se usa
+                            NodoInicial = true;
+                        //}
+                        
+                        NodoDar = MasIzquierdoDeDerecho(indice, Nodo, false);
+
+                        NodoAModificar.Valores.Add(NodoDar.Valores[0]);
+                        NodoAModificar.Valores.RemoveAt(indice);
+                        NodoDar.Valores.RemoveAt(0);
+
+                        NodoAModificar.Valores.Sort((x, y) => x.CompareTo(y));
+
+                        if (ExisteUnderFlow(NodoDar) == true)
+                        {
+                            //Llamo a un metodo para ver si un hermano VECINO puede prestar
+                            VerificarHermanos(NodoDar.Padre, NodoDar);
+                        }
+                    }
+                    else
+                    {
+                        Nodo.Valores.RemoveAt(i);
+                    }
+
+                    if (NodoInicial == true)
+                    {
+                        Nodo = NodoAModificar;
+                    }
+
+                    if (ExisteUnderFlow(Nodo) == true)
+                    {
+                        //Llamo a un metodo para ver si un hermano VECINO puede prestar
+                        VerificarHermanos(Nodo.Padre, Nodo);
+                    }
                 }
             }
-
 
             if (BEncontrado == false && Nodo.Hijos.Count > 0)
             {
                 NodoB<T> NodoHijo = new NodoB<T>();
                 NodoHijo = Nodo.Hijos[PosicionHijo(Nodo, valor)];
-                Busqueda(valor, NodoHijo);
-            }
-            else if (BEncontrado == false && Nodo.Hijos.Count == 0)
-            {
-                throw new NotImplementedException();
-            }
-            else if(BEncontrado == true && Nodo.Hijos.Count == 0) //Cuando el valor esta en una hoja
-            {
-                Nodo.Valores.RemoveAt(indice);
-            }
-            else if (BEncontrado == true && Nodo.Hijos.Count > 0) //Cuando el valor esta en una hoja
-            {
-                //ELIMINACION DE VALOR CON HIJOS
+                Eliminar(valor, NodoHijo);
             }
         }
 
 
+        public void VerificarHermanos(NodoB<T> padre, NodoB<T> nodo)
+        {
+
+            NodoB<T> hermano = new NodoB<T>();
+            int posicionvalorpadre = 0;
+            for (int i = 0; i < padre.Hijos.Count; i++)
+            {   //Si nodo es el primero y el hermano a la derecha puede prestar
+                if (padre.Hijos[i] == nodo && i == 0 && padre.Hijos[i + 1].Valores.Count > padre.min) 
+                {
+                    hermano = padre.Hijos[i + 1];
+                    posicionvalorpadre = i;
+                    break;
+                }
+                //Si nodo es el ultimo y el hermano a la izquierda puede prestar
+                else if (padre.Hijos[i] == nodo && i == (padre.Hijos.Count - 1) && padre.Hijos[i - 1].Valores.Count > padre.min) 
+                {
+                    hermano = padre.Hijos[i - 1];
+                    posicionvalorpadre = i - 1;
+                    break;
+                }
+                //Si el nodo se encuentra en medio, y el hermano a prestar es el de la izquierda
+                else if (padre.Hijos[i] == nodo && i > 0 && i < padre.Hijos.Count - 1 && padre.Hijos[i - 1].Valores.Count > padre.min) 
+                {
+                    hermano = padre.Hijos[i - 1];
+                    posicionvalorpadre = i - 1;
+                    break;
+                }
+                //Si el nodo se encuentra en medio, y el hermano a prestar es el de la derecha
+                else if (padre.Hijos[i] == nodo && i > 0 && i < padre.Hijos.Count - 1 && padre.Hijos[i + 1].Valores.Count > padre.min) 
+                {
+                    hermano = padre.Hijos[i + 1];
+                    posicionvalorpadre = i;
+                    break;
+                }
+                
+            }
+
+            //Si no encontro hermano para prestar valor
+            if(hermano.Padre == null)
+            {
+                for (int i = 0; i < padre.Hijos.Count; i++)
+                {
+                    //SI NO SE PUEDEN PRESTAR VALORES SOLO PASA LA POSICION DEL PADRE
+                    //Si el nodo que tiene underflow es el primero 
+                    if (padre.Hijos[i] == nodo && i == 0)
+                    {
+                        posicionvalorpadre = i;
+                        hermano = padre.Hijos[posicionvalorpadre + 1];
+                        break;
+                    }
+                    //Si el nodo que tiene underflow es el ultimo
+                    else if (padre.Hijos[i] == nodo && i == (padre.Hijos.Count - 1))
+                    {
+                        posicionvalorpadre = i - 1;
+                        hermano = padre.Hijos[posicionvalorpadre];
+                        break;
+                    }
+                    //Si el nodo que tiene underflow esta en medio
+                    else if (padre.Hijos[i] == nodo && i > 0 && i < padre.Hijos.Count - 1)
+                    {
+                        posicionvalorpadre = i;
+                        if(padre.Hijos[posicionvalorpadre + 1].Valores.Count > padre.Hijos[posicionvalorpadre - 1].Valores.Count)
+                        {
+                            hermano = padre.Hijos[posicionvalorpadre + 1];
+                        }
+                        else if (padre.Hijos[posicionvalorpadre + 1].Valores.Count < padre.Hijos[posicionvalorpadre - 1].Valores.Count)
+                        {
+                            hermano = padre.Hijos[posicionvalorpadre - 1];
+                        }
+                        else
+                        {
+                            hermano = padre.Hijos[posicionvalorpadre + 1];
+                        }
+                        break;
+                    }
+                }
+                
+                JuntarNodos(padre, posicionvalorpadre, nodo, hermano);
+            }
+            //Encontro valor y solo se traslada
+            else
+            {
+                TrasladarValor(hermano, posicionvalorpadre, padre, nodo);
+            }
+        }
+
+        public void TrasladarValor(NodoB<T> hijoEmisor, int posicionvalorpadre, NodoB<T> padre, NodoB<T> nodoReceptor)
+        {
+
+            //NODO HERMANO IZQUIERDO: Si debo de pasar el ultimo dato
+            if (hijoEmisor.Valores[hijoEmisor.Valores.Count - 1].CompareTo(padre.Valores[posicionvalorpadre]) < 0) //Hijo a la izquierda
+            {
+                padre.Valores.Add(hijoEmisor.Valores[hijoEmisor.Valores.Count - 1]); //Subir valor
+                hijoEmisor.Valores.Remove(hijoEmisor.Valores[hijoEmisor.Valores.Count - 1]); //quita valor que se paso a padre
+                nodoReceptor.Valores.Add(padre.Valores[posicionvalorpadre]); //bajar valor de padre
+                padre.Valores.Remove(padre.Valores[posicionvalorpadre]);
+                nodoReceptor.Valores.Sort((x, y) => x.CompareTo(y)); // ordenar hijo que recibe
+
+                //hijoEmisor.Valores.RemoveAt(hijoEmisor.Valores.Count - 1);
+
+                padre.Valores.Sort((x, y) => x.CompareTo(y));
+            }
+
+            //HIJO A LA DERECHA: Si debo de pasar el primer dato
+            else if (hijoEmisor.Valores[0].CompareTo(padre.Valores[posicionvalorpadre]) > 0)
+            {
+                padre.Valores.Add(hijoEmisor.Valores[0]);
+                nodoReceptor.Valores.Add(padre.Valores[posicionvalorpadre]);
+                nodoReceptor.Valores.Sort((x, y) => x.CompareTo(y));
+
+                hijoEmisor.Valores.Remove(hijoEmisor.Valores[0]);
+
+                padre.Valores.Remove(padre.Valores[posicionvalorpadre]);
+
+                padre.Valores.Sort((x, y) => x.CompareTo(y));
+            }
+
+        }
+
+        public void JuntarNodos(NodoB<T> padre, int posicionvalorpadre, NodoB<T> hijo, NodoB<T> hermano)
+        {
+
+            if (hijo.Valores[0].CompareTo(padre.Valores[padre.Valores.Count - 1]) > 0) //HIJO A LA DERECHA: Si es el ultimo nodo
+            {
+                hermano = padre.Hijos[posicionvalorpadre];
+                for (int i = 0; i < hermano.Valores.Count; i++)
+                {
+                    hijo.Valores.Add(hermano.Valores[i]);
+                }
+
+                if (ExisteNodosHijo(hijo) == true)
+                {
+                    for (int i = 0; i < hermano.Hijos.Count; i++)
+                    {
+                        hijo.Hijos.Add(hermano.Hijos[i]);
+                    }
+                }
+
+                padre.Hijos.RemoveAt(posicionvalorpadre); //Elimina el nodo hermano
+                hijo.Valores.Add(padre.Valores[posicionvalorpadre]); //Manda valor de raiz
+                hijo.Valores.Sort((x, y) => x.CompareTo(y));
+
+                padre.Valores.Remove(padre.Valores[posicionvalorpadre]);
+
+                if (padre.Valores.Count < padre.min)
+                {
+                    VerificarHermanos(padre.Padre, padre);
+                }
+            }
+
+            else if (hijo.Valores[hijo.Valores.Count - 1].CompareTo(padre.Valores[0]) < 0) //HIJO A LA IZQUIERDA DEL PADRE
+            {
+                hermano = padre.Hijos[posicionvalorpadre + 1];
+                for (int i = 0; i < hermano.Valores.Count; i++)
+                {
+                    hijo.Valores.Add(hermano.Valores[i]);
+                }
+
+                if (ExisteNodosHijo(hijo) == true)
+                {
+                    for (int i = 0; i < hermano.Hijos.Count; i++)
+                    {
+                        hijo.Hijos.Add(hermano.Hijos[i]);
+                    }
+                }
+
+                padre.Hijos.RemoveAt(posicionvalorpadre + 1); //Elimina el nodo hermano
+                hijo.Valores.Add(padre.Valores[0]); //Manda valor de raiz
+                hijo.Valores.Sort((x, y) => x.CompareTo(y));
+
+                padre.Valores.Remove(padre.Valores[0]);
+
+                if(padre.Valores.Count < padre.min && padre.Padre != null)
+                {
+                    VerificarHermanos(padre.Padre, padre);
+                }
+            }
+            
+            else if (hijo.Valores[0].CompareTo(padre.Valores[0]) > 0 && hijo.Valores[hijo.Valores.Count - 1].CompareTo(padre.Valores[padre.Valores.Count - 1]) < 0) //Si es uno de enmedio
+            {
+                if (hijo.Valores[0].CompareTo(padre.Valores[posicionvalorpadre]) > 0)
+                {
+                    hermano = padre.Hijos[posicionvalorpadre];
+                    for (int i = 0; i < hermano.Valores.Count; i++)
+                    {
+                        hijo.Valores.Add(hermano.Valores[i]);
+                    }
+
+                    if (ExisteNodosHijo(hijo) == true)
+                    {
+                        for (int i = 0; i < hermano.Hijos.Count; i++)
+                        {
+                            hijo.Hijos.Add(hermano.Hijos[i]);
+                        }
+                    }
+
+                    padre.Hijos.RemoveAt(posicionvalorpadre); //Elimina el nodo hermano
+                    hijo.Valores.Add(padre.Valores[padre.Valores.Count - 1]); //Manda valor de raiz
+                    hijo.Valores.Sort((x, y) => x.CompareTo(y));
+                }
+                else
+                {
+                    hermano = padre.Hijos[posicionvalorpadre + 1];
+                    for (int i = 0; i < hermano.Valores.Count; i++)
+                    {
+                        hijo.Valores.Add(hermano.Valores[i]);
+                    }
+
+                    if (ExisteNodosHijo(hijo) == true)
+                    {
+                        for (int i = 0; i < hermano.Hijos.Count; i++)
+                        {
+                            hijo.Hijos.Add(hermano.Hijos[i]);
+                        }
+                    }
+
+                    padre.Hijos.RemoveAt(posicionvalorpadre + 1); //Elimina el nodo hermano
+                    hijo.Valores.Add(padre.Valores[posicionvalorpadre]); //Manda valor de raiz
+                    padre.Valores.RemoveAt(posicionvalorpadre);
+                    hijo.Valores.Sort((x, y) => x.CompareTo(y));
+                }
+
+                
+                
+
+                
+            }
+            
+        }
+
+    public NodoB<T> MasIzquierdoDeDerecho(int IndiceValor, NodoB<T> Nodo, bool IrDerecha)
+    {
+        if (IrDerecha == false && Nodo.Hijos.Count > 0)
+        {
+            IrDerecha = true;
+            return MasIzquierdoDeDerecho(IndiceValor, Nodo.Hijos[IndiceValor + 1], IrDerecha);
+        }
+        else if (IrDerecha == true && Nodo.Hijos.Count > 0)
+        {
+            return MasIzquierdoDeDerecho(IndiceValor, Nodo.Hijos[0], IrDerecha);
+        }
+        else if (IrDerecha == true && Nodo.Hijos.Count == 0)
+        {
+            return Nodo;
+        }
+        else
+        {
+            return Nodo;
+        }
+
+    }
+
+
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+           throw new NotImplementedException();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
